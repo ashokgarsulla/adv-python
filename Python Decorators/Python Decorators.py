150df2b428d10f9d04faa12aa800a2bc1b7300d7
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-16 14:43:53 +0530

    Implemented Chaining decorator

--- Python Decorators/Python Decorators.py
+++ Python Decorators/Python Decorators.py
@@ -73,3 +73,30 @@ print("-" *30,'LINE 69',"-" *30)
 def divide(a, b):
     print(a/b)
 divide(3,2)
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Chaining Decorators in Python
+# -----------------------------------------------------------------------------------------------------------------------
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 30)
+        func(*args, **kwargs)
+        print("*" * 30)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 30)
+        func(*args, **kwargs)
+        print("%" * 30)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+    
+printer("Hello")
+# -----------------------------------------------------------------------------------------------------------------------
