8313b185f7720a8d668f68156db08253a6b28e4f
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-16 13:58:10 +0530

    adv python content

---
+++ Python Closures/Defining a Closure Function.py
@@ -0,0 +1,14 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    return printer  # returns the nested function
+
+
+# Now let's try calling this function.
+# Output: Hello
+another = print_msg("Hello")
+another()
