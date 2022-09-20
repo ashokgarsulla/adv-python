df7af089ef7720fbce7fceddae62892aff3399f3
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-20 10:06:02 +0530

    actual code of for loop

--- Python Iterators/Python Iterators.py
+++ Python Iterators/Python Iterators.py
@@ -34,3 +34,38 @@ print(my_iter.__next__())
 print("*"*20,"LINE 35","*"*20)
 for i in my_list:
     print(i)
+
+# //////////////////////////////////////////////////////////////////////////
+# Working of for loop for Iterators
+# //////////////////////////////////////////////////////////////////////////
+print("*"*20,"LINE 42","*"*20)
+test_list = [1,2,3,4,5]
+for element in test_list:
+    print(element)
+
+#
+
+print("*"*20,"LINE 49","*"*20)
+# create an iterator object from that iterable
+iter_obj = iter(test_list)
+
+# infinite loop
+while True:
+    try:
+        # get the next item
+        element = next(iter_obj)
+        # do something with element
+        print(element)
+    except StopIteration:
+        # if StopIteration is raised, break from loop
+        break
+# -------------------------- NOTES START ----------------------------------------------------------
+# So internally, the for loop creates an iterator object, iter_obj by calling iter() on the iterable.
+
+# Ironically, this for loop is actually an infinite while loop.
+
+# Inside the loop, it calls next() to get the next element and executes the body of the for loop with
+#  this value. After all the items exhaust, StopIteration is raised which is internally caught and the loop ends. 
+#  Note that any other kind of exception will pass through.
+        break
+# -------------------------- NOTES END ----------------------------------------------------------
