05c5cf687c45f5f18df8ef32163d475eafcd24ed
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-21 21:08:25 +0530

    generator function using manually statement yield

--- Python Generators/Python Generators.py
+++ Python Generators/Python Generators.py
@@ -5,6 +5,9 @@
 # It is as easy as defining a normal function, but with a "yield" statement instead of a "return" statement
 
 # Generating Even number using custome iterator
+from tkinter import N
+
+
 class Even:
     def __init__(self,max):
         self.n = 2
@@ -37,3 +40,60 @@ print(my_iter.__next__())
 print(my_iter.__next__())
 print(my_iter.__next__())
 print(my_iter.__next__())
+
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+# Create Generators in Python
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+# ------------ IMPORTANT POINTS  ---------------
+# * It is as easy as defining a normal function, but with a yield statement instead of a return statement
+
+# * If a function contains at least one yield statement (it may contain other yield or return statements), 
+# it becomes a generator function. Both yield and return will return some value from a function.
+# * The difference is that while a return statement terminates a function entirely, yield statement 
+# pauses the function saving all its states and later continues from there on successive calls.
+
+def even_generator():
+    n = 0
+
+    n += 2
+    yield n
+
+    n += 2
+    yield n
+
+    
+    n += 2
+    yield n
+
+    n += 2
+    yield n
+
+    n += 2
+    yield n
+
+    n += 2
+    yield n
+
+print("*"*20,"LINE 80","*"*20)
+# It returns an object but does not start execution immediately.
+gen_obj = even_generator()
+
+for i in gen_obj:
+    print(i)
+
+print("*"*20,"LINE 86","*"*20)
+my_gen_obj = even_generator()
+
+# We can iterate through the items using next().
+print(my_gen_obj.__next__())
+print(my_gen_obj.__next__())
+print(my_gen_obj.__next__())
+# we can also write
+print(next(my_gen_obj))
+print(next(my_gen_obj))
+print(next(my_gen_obj))
+
+# when the function terminates, StopIteration is raised automatically on further calls
+# // It will give StopIteration  bcz:Finally, 
+# print(next(my_gen_obj)) 
