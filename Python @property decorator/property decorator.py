778957605e02e0d3b2c412d2214c3f34245a28cb
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-27 21:54:52 +0530

    through  property object get and set value

--- Python @property decorator/property decorator.py
+++ Python @property decorator/property decorator.py
@@ -10,6 +10,9 @@
 # Class Without Getters and Setters
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # Basic method of setting and getting attributes in Python
+from tempfile import TemporaryDirectory
+
+
 class Celsius:
     def __init__(self, temperature=0):
         self.temperature = temperature
@@ -79,4 +82,53 @@ print(human.to_fahrenheit())
 
 
 
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# The property Class
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+# using property class
+class Celsius:
+    def __init__(self, temperature=0):
+        self.temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    # getter
+    def get_temperature(self):
+        print("Getting value...")
+        return self._temperature
+
+    # setter
+    def set_temperature(self, value):
+        print("Setting value...")
+        if value < -273.15:
+            raise ValueError("Temperature below -273.15 is not possible")
+        self._temperature = value
+
+    # creating a property object
+    temperature = property(get_temperature, set_temperature)
+
+
+
+print("*"*20,"LINE 112","*"*20)
+human = Celsius(37)
+
+print(human.temperature)
+
+print(human.to_fahrenheit())
+
+# It will give value error
+# human.temperature = -300
+
+human.temperature = 75
+print(human.temperature)
+print(human.to_fahrenheit())
+
+# ------------------------------------------- NOTE --------------------------------------------------------------------
+# self.temperature = temperature. This expression automatically calls set_temperature().
+# self.temperature. This expression automatically calls set_temperature().
+# c = Celcius(30)
+# c.temperature automatically calls get_temperature()
+# Note: The actual temperature value is stored in the private _temperature variable. 
+# The temperature attribute is a property object which provides an interface to this private variable.
