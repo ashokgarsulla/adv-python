5b7be00b9f1b8116d2be5b38c441a8600f231bc4
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-27 21:40:44 +0530

    class using getter and setter

--- Python @property decorator/property decorator.py
+++ Python @property decorator/property decorator.py
@@ -36,6 +36,46 @@ print(human.__dict__)
 
 
 
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Using Getters and Setters
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Making Getters and Setter methods
+class Celsius:
+    def __init__(self, temperature=0):
+        self.set_temperature(temperature)
+
+    def to_fahrenheit(self):
+        return (self.get_temperature() * 1.8) + 32
+
+    # getter method
+    def get_temperature(self):
+        return self._temperature
+
+    # setter method
+    def set_temperature(self, value):
+        if value < -273.15:
+            raise ValueError("Temperature below -273.15 is not possible.")
+        self._temperature = value
+
+print("*"*20,"LINE 61","*"*20)
+
+# Create a new object, set_temperature() internally called by __init__
+human = Celsius(37)
+
+# Get the temperature attribute via a getter
+print(human.get_temperature())
+
+# Get the to_fahrenheit method, get_temperature() called by the method itself
+print(human.to_fahrenheit())
+
+# new constraint implementation 
+# updated temprature 
+human.set_temperature(55)
+print(human.get_temperature())
+
+# Get the to_fahreheit method
+print(human.to_fahrenheit())
 
 
 
