f06a5e07cb40f23d175e497d53938dce950ea1d8
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-28 20:19:11 +0530

    match group

--- Python RegEx/Python RegEx.py
+++ Python RegEx/Python RegEx.py
@@ -143,3 +143,28 @@ else:
 
 # Output: pattern found inside the string
 
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Match object
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# match.group() : The group() method returns the part of the string where there is a match.
+
+print("*"*20,"LINE 153","*"*20)
+
+import re
+
+string = '39801 356, 2102 1111'
+
+# Three digit number followed by space followed by two digit number
+pattern = '(\d{3}) (\d{2})'
+
+# match variable contains a Match object.
+match = re.search(pattern, string) 
+
+if match:
+  print(match.group())
+else:
+  print("pattern not found")
+
+# Output: 801 35
