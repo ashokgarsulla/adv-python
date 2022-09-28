e3406968452ba45b45f915b9142b183d7e36e750
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-28 20:12:33 +0530

    count substitions

--- Python RegEx/Python RegEx.py
+++ Python RegEx/Python RegEx.py
@@ -99,3 +99,27 @@ new_string = re.sub(pattern, replace, string)
 print(new_string)
 
 # Output: abc12de23f456
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# re.subn(): The re.subn() is similar to re.sub() except it returns a tuple of 2 items containing
+#  the new string and the number of substitutions made.
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Program to remove all whitespaces
+import re
+
+# multiline string
+string = 'abc 12\
+de 23 \n f45 6'
+
+# matches all whitespace characters
+pattern = '\s+'
+
+# empty string
+replace = ''
+
+new_string = re.subn(pattern, replace, string) 
+print(new_string)
+
+# Output: ('abc12de23f456', 4)
