3a617f89841b9a8bea6f1075a8cb3e043fd8a40e
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-28 20:06:56 +0530

    remove all white space

--- Python RegEx/Python RegEx.py
+++ Python RegEx/Python RegEx.py
@@ -77,3 +77,25 @@ print(result)
 
 # Output: ['Twelve:', ' Eighty nine:89 Nine:9.']
 
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# re.sub()
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+print("*"*20,"LINE 84","*"*20)
+# Program to remove all whitespaces
+import re
+
+# multiline string
+string = 'abc 12\
+de 23 \n f45 6'
+
+# matches all whitespace characters
+pattern = '\s+'
+
+# empty string
+replace = ''
+
+new_string = re.sub(pattern, replace, string) 
+print(new_string)
+
+# Output: abc12de23f456
