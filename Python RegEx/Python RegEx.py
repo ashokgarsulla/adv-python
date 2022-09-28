5cdee5883d8ae838bfb0c1e4ca0fca9d989d4e7f
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-28 20:03:35 +0530

    split only at first occ

--- Python RegEx/Python RegEx.py
+++ Python RegEx/Python RegEx.py
@@ -61,3 +61,19 @@ result = re.split(pattern, string)
 print(result)
 
 # Output: ['Twelve:', ' Eighty nine:', '.']
+
+
+# split only at first occurence
+print("*"*20,"LINE 68","*"*20) 
+import re
+
+string = 'Twelve:12 Eighty nine:89 Nine:9.'
+pattern = '\d+'
+
+# maxsplit = 1
+# split only at the first occurrence
+result = re.split(pattern, string, 1) 
+print(result)
+
+# Output: ['Twelve:', ' Eighty nine:89 Nine:9.']
+
