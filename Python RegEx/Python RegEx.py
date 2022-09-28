d6b9858f437e845273d7320c93b574ec33ace82e
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-28 22:13:52 +0530

    Raw string using r prefix

--- Python RegEx/Python RegEx.py
+++ Python RegEx/Python RegEx.py
@@ -183,3 +183,26 @@ else:
   print("pattern not found")
 
 # Output: 801 35
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Using r prefix before RegEx
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# When r or R prefix is used before a regular expression, it means raw string. For example, '\n' is a new line 
+# whereas r'\n' means two characters: a backslash \ followed by n.
+
+# Backlash \ is used to escape various characters including all metacharacters. 
+# However, using r prefix makes \ treat as a normal character.
+
+# Raw string using r prefix
+print("*"*20,"LINE 200","*"*20)
+import re
+
+string = '\n and \r are escape sequences.'
+
+print(string)
+
+result = re.findall(r'[\n\r]', string) 
+print(result)
+
+# Output: ['\n', '\r']
