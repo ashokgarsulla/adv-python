3ffd5a45d4e0a64a1250914c809d5ee3dce1ceed
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-07 19:42:23 +0530

    Print today's year, month and day

--- Python Date & Time/Python datetime.py
+++ Python Date & Time/Python datetime.py
@@ -50,3 +50,18 @@ print(d)
 print("-" *30,'LINE 50',"-" *30)
 timestamp = date.fromtimestamp(1326244364)
 print("Date =", timestamp)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Print today's year, month and day
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+from datetime import date
+
+# date object of today's date
+today = date.today() 
+
+print("-" *30,'LINE 65',"-" *30)
+print("Current year:", today.year)
+print("Current month:", today.month)
+print("Current day:", today.day)
