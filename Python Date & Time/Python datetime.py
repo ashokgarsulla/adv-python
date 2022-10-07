9e0d71b0ccb2ef84f12efc4eae5d31dfd5af25da
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-07 21:27:07 +0530

    Difference between two timedelta objects

--- Python Date & Time/Python datetime.py
+++ Python Date & Time/Python datetime.py
@@ -170,3 +170,15 @@ print("t6 =", t6)
 
 print("type of t3 =", type(t3)) 
 
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Difference between two timedelta objects
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+print("-" *30,'LINE 177',"-" *30)
+
+from datetime import timedelta
+
+t1 = timedelta(weeks = 2, days = 5, hours = 1, seconds = 33)
+t2 = timedelta(days = 4, hours = 11, minutes = 4, seconds = 54)
+t3 = t1 - t2
+
+print("t3 =", t3)
