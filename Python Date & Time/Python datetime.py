2f226b4df56c416037bff8a61e93595dcfe0bd8f
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-07 21:30:01 +0530

    printing negative time delta object

--- Python Date & Time/Python datetime.py
+++ Python Date & Time/Python datetime.py
@@ -182,3 +182,19 @@ t2 = timedelta(days = 4, hours = 11, minutes = 4, seconds = 54)
 t3 = t1 - t2
 
 print("t3 =", t3)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Printing negative timedelta object
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+from datetime import timedelta
+
+t1 = timedelta(seconds = 33)
+t2 = timedelta(seconds = 54)
+t3 = t1 - t2
+
+
+print("-" *30,'LINE 198',"-" *30)
+print("t3 =", t3)
+print("t3 =", abs(t3))
