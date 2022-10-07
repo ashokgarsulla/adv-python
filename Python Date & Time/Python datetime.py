39f94c8ca57c79c13d27951213104e9f2b8483f9
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-07 21:21:45 +0530

    difference between two date and time

--- Python Date & Time/Python datetime.py
+++ Python Date & Time/Python datetime.py
@@ -165,7 +165,7 @@ print("t3 =", t3)
 
 t4 = datetime(year = 2018, month = 7, day = 12, hour = 7, minute = 9, second = 33)
 t5 = datetime(year = 2019, month = 6, day = 10, hour = 5, minute = 55, second = 13)
-t6 = t4 - t5
+t6 = t4 - t5 
 print("t6 =", t6)
 
 print("type of t3 =", type(t3)) 
