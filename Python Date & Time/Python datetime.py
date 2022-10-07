85082bd648556ff3b4d159cf570a963e7f072ff3
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-07 21:20:37 +0530

    time delta

--- Python Date & Time/Python datetime.py
+++ Python Date & Time/Python datetime.py
@@ -145,3 +145,28 @@ print("month =", a.month)
 print("hour =", a.hour)
 print("minute =", a.minute)
 print("timestamp =", a.timestamp())
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# datetime.timedelta
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# A timedelta object represents the difference between two dates or times.
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Difference between two dates and times
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+from datetime import datetime, date
+print("-" *30,'LINE 160',"-" *30)
+
+t1 = date(year = 2018, month = 7, day = 12)
+t2 = date(year = 2017, month = 12, day = 23)
+t3 = t1 - t2
+print("t3 =", t3)
+
+t4 = datetime(year = 2018, month = 7, day = 12, hour = 7, minute = 9, second = 33)
+t5 = datetime(year = 2019, month = 6, day = 10, hour = 5, minute = 55, second = 13)
+t6 = t4 - t5
+print("t6 =", t6)
+
+print("type of t3 =", type(t3)) 
+
