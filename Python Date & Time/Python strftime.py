3cd773f8b5afd8fb3b1b1176b06bee469902d02f
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-08 23:41:15 +0530

    Creating string from a timestamp

--- Python Date & Time/Python strftime.py
+++ Python Date & Time/Python strftime.py
@@ -27,3 +27,29 @@ print("time:", time)
 
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 print("date and time:",date_time)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Creating string from a timestamp
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+from datetime import datetime
+
+timestamp = 1528797322
+date_time = datetime.fromtimestamp(timestamp)
+
+print("-" *30,'LINE 42',"-" *30)
+print("Date time object:", date_time)
+
+d = date_time.strftime("%m/%d/%Y, %H:%M:%S")
+print("Output 2:", d)	
+
+d = date_time.strftime("%d %b, %Y")
+print("Output 3:", d)
+
+d = date_time.strftime("%d %B, %Y")
+print("Output 4:", d)
+
+d = date_time.strftime("%I%p")
+print("Output 5:", d)
