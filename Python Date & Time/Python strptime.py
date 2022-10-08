434b2ffc9910f646508a8e3c4a06a0c702052aac
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-08 23:57:00 +0530

    string to datetime object with full time and date

--- Python Date & Time/Python strptime.py
+++ Python Date & Time/Python strptime.py
@@ -19,3 +19,22 @@ date_object = datetime.strptime(date_string, "%d %B, %Y")
 
 print("date_object =", date_object)
 print("type of date_object =", type(date_object))
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#  string to datetime object with full time and date 
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+from datetime import datetime
+
+dt_string = "12/11/2018 09:15:32"
+
+
+print("-" *30,'LINE 34',"-" *30)
+# Considering date is in dd/mm/yyyy format
+dt_object1 = datetime.strptime(dt_string, "%d/%m/%Y %H:%M:%S")
+print("dt_object1 =", dt_object1)
+
+# Considering date is in mm/dd/yyyy format
+dt_object2 = datetime.strptime(dt_string, "%m/%d/%Y %H:%M:%S")
+print("dt_object2 =", dt_object2)
