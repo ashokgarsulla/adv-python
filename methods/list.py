4a841a1eab91ea0d74d7f7f6808fe5b8001934e8
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-09 22:10:33 +0530

    remove

--- methods/list.py
+++ methods/list.py
@@ -22,3 +22,9 @@ print(animal_foods)
 print(animals)
 animals.insert(3,"Aadmi")
 print(animals)
+
+# remove : remove matchiong element
+
+print(animal_foods)
+animal_foods.remove("dog")
+print(animal_foods)
